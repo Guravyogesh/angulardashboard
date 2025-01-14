55b2c198b3872085b28f0b79feb9f66216b24e85
Guravyogesh <yogeshgurav0918@gmail.com>
2025-01-14 09:57:47 +0530

    all changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
+declare var bootstrap: any;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, SidebarComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'transact';
+  title = 'admin-dashboard';
+  isSidebarClosed = false;
+  handleSidebarToggle(isClosed: boolean) {
+    this.isSidebarClosed = isClosed;
+  }
+  openSettingsModal() {
+    const settingsModal = new bootstrap.Modal(document.getElementById('settingsModal'), {
+      keyboard: false
+    });
+    settingsModal.show();
+  }
 }
